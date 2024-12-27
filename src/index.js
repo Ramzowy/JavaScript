5c282d8034c1c6c34d655c6d15619ccee1a68e25
Rahim <ramzowy@gmail.com>
2024-12-27 15:53:52 -0500

    Few changes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import http from 'http';
 const requestHandler = function (req, res) {
   if (req.method === 'POST' && req.url === '/users') {  
       res.statusCode = 400;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ message: 'Payload should not be empty' }));
+      res.end();
       return;
   }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('I love Cheese!');
+  res.end('I hate CHEESE');
 };
 
 const server = http.createServer(requestHandler);
