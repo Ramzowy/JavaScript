7a54cd6ee9b3b10059ad40fb3d22008fb8d3c304
Rahim <ramzowy@gmail.com>
2024-12-27 13:01:03 -0500

    Few changes

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -2,21 +2,21 @@
   
   import superagent from 'superagent';
   import { When, Then } from 'cucumber';
-
+  
   let request;
   let result;
   let error;
-
+  
   When('the client creates a POST request to /users', function () {
     request = superagent('POST', 'http://localhost:8080/users');
   });
-
+  
   When('attaches a generic empty payload', function () {
-    return undefined;
-    });
-
-    When('sends the request', function (callback) {
-      request
+    request.send({});  // Send an empty object as the payload
+  });
+  
+  When('sends the request', function (callback) {
+    request
       .then((response) => {
         result = response.res;
         callback();
@@ -25,21 +25,23 @@
         error = errResponse.response;
         callback();
       });
-    });
-
-   Then('our API should respond with a 400 HTTP status code', function ()
-   {
-    if (error.statusCode !== 400) {
-      throw new Error();
+  });
+  
+  Then('our API should respond with a 400 HTTP status code', function () {
+    if (result.statusCode !== 400) {
+      throw new Error('Expected status code 400 but got ' + result.statusCode);
     }
   });
-
-   Then('the payload of the response should be a JSON object', function
-   (callback) {
-   callback(null, 'pending');
-   });
-
-   Then('contains a message property which says "Payload should not be empty"', function
-    (callback) {
-     callback(null, 'pending');
-   });
+  
+  Then('the payload of the response should be a JSON object', function () {
+    if (typeof result.body !== 'object') {
+      throw new Error('Response body is not a JSON object');
+    }
+  });
+  
+  Then('contains a message property which says "Payload should not be empty"', function () {
+    if (result.body.message !== 'Payload should not be empty') {
+      throw new Error('Expected message "Payload should not be empty" but got: ' + result.body.message);
+    }
+  });
+  
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@
 
 import "@babel/polyfill";
 import http from 'http';
+
 const requestHandler = function (req, res) {
-  if (req.method === 'POST' && req.url === 'http://localhost:8080/users') {  
-      res.statusCode = 400; 
-      res.end();  
-         return; 
-       }
-  res.writeHead(200, {'Content-Type': 'text/plain'}); 
-  res.end('I love Cheese!');}
+  if (req.method === 'POST' && req.url === '/users') {  
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ message: 'Payload should not be empty' }));
+      return;
+  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('I love Cheese!');
+};
+
 const server = http.createServer(requestHandler);
 server.listen(8080);
-
