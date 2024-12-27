0cd569d91602064932b157ea0a642f94578450f6
Rahim <ramzowy@gmail.com>
2024-12-27 11:50:24 -0500

    Made file changes by creating API SERVER

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,23 +1,38 @@
 
+  
+  import superagent from 'superagent';
   import { When, Then } from 'cucumber';
 
-  When ('the client creates a POST request to /users', function
-  (callback) {
-    callback(null, 'pending');
-   });
+  let request;
+  let result;
+  let error;
 
-   When('attaches a generic empty payload', function (callback) {
-      callback(null, 'pending');
-   });
+  When('the client creates a POST request to /users', function () {
+    request = superagent('POST', 'localhost:8080/users');
+  });
 
-   When('sends the request', function (callback) {
-     callback(null, 'pending');
+  When('sends the request', function (callback) {
+    return undefined;
     });
 
-   Then('our API should respond with a 400 HTTP status code', function
-   (callback) {
-    callback(null, 'pending');
-   });
+    When('sends the request', function (callback) {
+      request
+      .then((response) => {
+        result = response.res;
+        callback();
+      })
+      .catch((errResponse) => {
+        error = errResponse.response;
+        callback();
+      });
+    });
+
+   Then('our API should respond with a 400 HTTP status code', function ()
+   {
+    if (error.statusCode !== 400) {
+      throw new Error();
+    }
+  });
 
    Then('the payload of the response should be a JSON object', function
    (callback) {
